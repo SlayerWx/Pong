8c873e894394986262eaaa212d050d5605778c1e
SlayerWx <slayerwx95@gmail.com>
2019-09-17 06:34:30 -0300

    scripts de la anterior version para guia

---
+++ Pong old version/Main.cpp
@@ -0,0 +1,23 @@
+//Ruffo Maximiliano
+#include "raylib.h"
+#include "Player.h"
+using namespace std;
+
+void main()
+{
+	// Initialization
+//--------------------------------------------------------------------------------------
+	SetTargetFPS(60);
+	const int screenWidth = 800;
+	const int screenHeight = 450;
+	InitWindow(screenWidth, screenHeight, "PONG - By Ruffo Maximiliano");
+	const int playerHeightSize = 100;
+	const int playerWidthSize = 20;
+	const float PlayersSpeed = 5.2f;
+	const float normalSpeedBallon = 9.0f;
+	const int RadiusBallon = 15;
+	while (true)
+	{
+
+	}
+}
